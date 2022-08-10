9c925defb952c0ed22cc1cbea4fb44c076adf135
iheehee <iheeheedev@gmail.com>
2022-08-10 15:37:02 +0900

    finish

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import { StatusBar } from "expo-status-bar";
-import { useEffect, useState } from "react";
+import * as Location from "expo-location";
+import React, { useEffect, useState } from "react";
 import {
   ScrollView,
   StyleSheet,
@@ -8,16 +8,26 @@ import {
   Text,
   View,
 } from "react-native";
-import * as Location from "expo-location";
+import { Fontisto } from "@expo/vector-icons";
 
 const { width: SCREEN_WIDTH } = Dimensions.get("window");
 const API_KEY = "aefd8b1596f876ac468978c3dae8ba89";
+
+const icons = {
+  Clouds: "cloudy",
+  Clear: "day-sunny",
+  Atmosphere: "cloudy-gusts",
+  Snow: "snow",
+  Rain: "rains",
+  Drizzle: "rain",
+  Thunderstorm: "lightning",
+};
 
 export default function App() {
   const [city, setCity] = useState("Loading");
   const [days, setDays] = useState([]);
   const [ok, setOk] = useState(true);
-  const ask = async () => {
+  const getWeather = async () => {
     const { granted } = await Location.requestForegroundPermissionsAsync();
     if (!granted) {
       setOk(false);
@@ -34,11 +44,12 @@ export default function App() {
       `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
     );
     const json = await response.json();
-    setDays(json.list[0]);
+
+    setDays(json.list);
   };
 
   useEffect(() => {
-    ask();
+    getWeather();
   }, []);
   return (
     <View style={styles.container}>
@@ -58,10 +69,26 @@ export default function App() {
         ) : (
           days.map((day, index) => (
             <View key={index} style={styles.day}>
-              <Text style={style.temp}>
-                {parserFloat(day.temp.day).toFixed(1)}
-              </Text>
-              <Text style={style.description}>{(day.weather[0], main)}</Text>
+              <View
+                style={{
+                  flexDirection: "row",
+                  alignItems: "center",
+                  justifyContent: "space-between",
+                  width: "100%",
+                }}
+              >
+                <Text style={styles.temp}>
+                  {parseFloat(day.main.temp).toFixed(1)}
+                </Text>
+                <Fontisto
+                  style={styles.icon}
+                  name={icons[day.weather[0].main]}
+                  size={68}
+                  color="white"
+                />
+              </View>
+              <Text style={styles.description}>{day.weather[0].main}</Text>
+              <Text style={styles.tinyText}>{day.dt_txt}</Text>
             </View>
           ))
         )}
@@ -82,18 +109,31 @@ const styles = StyleSheet.create({
   },
   cityName: {
     fontSize: 50,
-    fontWeight: "500",
+    color: "white",
   },
   weather: {},
   day: {
     width: SCREEN_WIDTH,
-    alignItems: "center",
+    alignItems: "left",
   },
   temp: {
+    marginLeft: 15,
     marginTop: 50,
-    fontSize: 178,
+    fontSize: 90,
+    color: "white",
   },
   description: {
-    fontSize: 50,
+    marginLeft: 25,
+    fontSize: 30,
+    color: "white",
+  },
+  tinyText: {
+    marginLeft: 25,
+    marginTop: 5,
+    fontSize: 17,
+    color: "white",
+  },
+  icon: {
+    marginRight: 25,
   },
 });
